533425e4b4e3ea15a766ef852effa06526630262
MinjoonCho <jaydencho0324@gmail.com>
2026-08-29 15:49:52 +0900

    feat: 공고 에러/성공 코드 구현

---
+++ src/main/java/com/sinchonthon/team5/odyssey/jobpost/JobPostSuccessCode.java
@@ -0,0 +1,39 @@
+package com.sinchonthon.team5.odyssey.jobpost;
+
+import com.sinchonthon.team5.odyssey.global.code.BaseSuccessCode;
+
+import org.springframework.http.HttpStatus;
+
+public enum JobPostSuccessCode implements BaseSuccessCode {
+
+    CREATED(HttpStatus.CREATED, "JOBPOST_201", "공고 등록 성공"),
+    LIST_FOUND(HttpStatus.OK, "JOBPOST_200_1", "공고 목록 조회 성공"),
+    DETAIL_FOUND(HttpStatus.OK, "JOBPOST_200_2", "공고 상세 조회 성공"),
+    MY_LIST_FOUND(HttpStatus.OK, "JOBPOST_200_3", "내 공고 목록 조회 성공"),
+    UPDATED(HttpStatus.OK, "JOBPOST_200_4", "공고 수정 성공");
+
+    private final HttpStatus status;
+    private final String code;
+    private final String message;
+
+    JobPostSuccessCode(HttpStatus status, String code, String message) {
+        this.status = status;
+        this.code = code;
+        this.message = message;
+    }
+
+    @Override
+    public HttpStatus getStatus() {
+        return status;
+    }
+
+    @Override
+    public String getCode() {
+        return code;
+    }
+
+    @Override
+    public String getMessage() {
+        return message;
+    }
+}
