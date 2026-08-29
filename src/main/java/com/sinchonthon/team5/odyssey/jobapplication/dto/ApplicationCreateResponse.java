56b2e21778650190d19d7482f05379ce8d9d5923
uaeoon <hhaaee9153@g.hongik.ac.kr>
2026-08-29 15:44:21 +0900

    feat: 공고 생성 응답 dto 구현

---
+++ src/main/java/com/sinchonthon/team5/odyssey/jobapplication/dto/ApplicationCreateResponse.java
@@ -0,0 +1,13 @@
+package com.sinchonthon.team5.odyssey.jobapplication.dto;
+
+import com.sinchonthon.team5.odyssey.jobapplication.enums.JobApplicationStatus;
+
+import java.time.OffsetDateTime;
+
+public record ApplicationCreateResponse(
+        Long applicationId,
+        Long jobPostId,
+        JobApplicationStatus status,
+        OffsetDateTime appliedAt
+) {
+}
