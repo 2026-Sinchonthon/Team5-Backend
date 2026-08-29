49dbbecc8f219eae04d03a732cdf116e5f7cb2a9
uaeoon <hhaaee9153@g.hongik.ac.kr>
2026-08-29 13:24:51 +0900

    chore: initialize Spring Boot project

---
+++ src/test/java/sinchonton_team5/_dyssey/ApplicationTests.java
@@ -0,0 +1,13 @@
+package sinchonton_team5._dyssey;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
